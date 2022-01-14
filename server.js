7ea5ef61a76dba132f7c5e193a4dae7b33feea88
Lars <79022337+LarsChristianK@users.noreply.github.com>
2022-01-14 20:55:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const wss = new WebS.Server({port:8081})
 wss.on("connection",ws=>{
     console.log("connection!")
     ws.on("message",msg=>{
-        wss.broadcast(JSON.stringify({func:msg}))
+        wss.broadcast(JSON.stringify({func:msg.toString()}))
     })
 });
 
